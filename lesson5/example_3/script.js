096d2b1882f970f1557768c92f855282bc81775c
Xizl <polzer@mail.ru>
2020-04-15 19:32:08 +0300

    Lesson5 (#4)
    * lesson4
    
    * lesson 5
    
    * chess change

---
+++ lesson5/example_3/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let showModelWindowButton = document.querySelector('button');
+let hiddenWindow = document.querySelector('.modal-window');
+
+showModelWindowButton.addEventListener('click', function (event) {
+    hiddenWindow.style.display = 'flex';
+})
